c739d3da36563f87c591037e84eb01fab9e0f61f
RealTiraqmisu <66070228@kmitl.ac.th>
2025-02-17 21:23:40 +0700

    finish appointment

--- front/client/index.js
+++ front/client/index.js
@@ -10,11 +10,11 @@ function showpop(){
     document.getElementById("popup-bg").style.opacity = 1;
     document.getElementById("popup-ov").style.opacity = 1;
 }
+const sel1 = document.getElementById("sel1");
+const sel2 = document.getElementById("sel2");
+const sel3 = document.getElementById("sel3");
+const sel4 = document.getElementById("sel4");
 function checkshowpop(){
-    const sel1 = document.getElementById("sel1");
-    const sel2 = document.getElementById("sel2");
-    const sel3 = document.getElementById("sel3");
-    const sel4 = document.getElementById("sel4");
     const div = document.createElement("div");
     div.id = "check-result";
     const table = document.createElement("table");
@@ -33,11 +33,6 @@ function checkshowpop(){
     table.appendChild(thead);
     div.appendChild(table);
 
-    // const button = document.createElement("button");
-    // button.type = "button";
-    // button.textContent = "นัดหมาย";
-    // button.className = "btn-pop";
-
     const divbtn = document.createElement("div");
     divbtn.id = "check-btn";
     divbtn.innerHTML = '<button class="btn-pop" id="btn-pop" onclick="gotoapp()">นัดหมาย</button>';
@@ -45,10 +40,9 @@ function checkshowpop(){
 
     document.getElementById("content").innerHTML = "";
     document.getElementById("content").appendChild(div);
-    // if (sel1.value != "" & sel2.value != "" & sel3.value != "" & sel4.value != ""){
-    //     showpop()
-    // }
-    showpop();
+    if (sel1.value != "" & sel2.value != "" & sel3.value != "" & sel4.value != ""){
+        showpop()
+    }
 }
 function showpromotion(){
     document.getElementById("content").innerHTML = "";
@@ -56,10 +50,10 @@ function showpromotion(){
 }
 function gotoapp(){
     // ไว้ส่ง value ของ dropdown ไปหน้า appointment
-    const sel1 = document.getElementById("sel1").value;
-    const sel2 = document.getElementById("sel2").value;
-    const sel3 = document.getElementById("sel3").value;
-    const sel4 = document.getElementById("sel4").value;
-    const letsend = `appointment.html?option1=${encodeURIComponent(sel1)}&option2=${encodeURIComponent(sel2)}&option3=${encodeURIComponent(sel3)}&option4=${encodeURIComponent(sel4)}`;
+    // const sel1 = document.getElementById("sel1").value;
+    // const sel2 = document.getElementById("sel2").value;
+    // const sel3 = document.getElementById("sel3").value;
+    // const sel4 = document.getElementById("sel4").value;
+    const letsend = `appointment.html?option1=${encodeURIComponent(sel1.value)}&option2=${encodeURIComponent(sel2.value)}&option3=${encodeURIComponent(sel3.value)}&option4=${encodeURIComponent(sel4.value)}`;
     window.location.href = letsend;
 }
