bf44c7e69610e7d9cfda9fe569d2052f7c817b9f
cj <145112109+66070248cj@users.noreply.github.com>
2025-02-22 14:40:38 +0700

    update em_history.html
    - add delete confirmation popup

--- front/client/em_history.js
+++ front/client/em_history.js
@@ -12,17 +12,29 @@ function bac2(){
     document.getElementById("popup-edit").style.opacity = 0;
     document.getElementById("popup-ov").style.opacity = 0;
 }
+//กดปิด Popup delete
+function bac3(){
+    document.getElementById("popup-delete").style.visibility = "hidden";
+    document.getElementById("popup-ov").style.visibility = "hidden";
+    document.getElementById("popup-delete").style.opacity = 0;
+    document.getElementById("popup-ov").style.opacity = 0;
+}
 
-//ปุ่มันทึกข้อมูล
+//ปุ่มบันทึกข้อมูล
 function save_info(){
     alert("บันทึกข้อมูลสำเร็จ")
 }
-//ปุ่มลบข้อมูล
-function delete_info(){
+//ปุ่มยกเลิกการลบข้อมูล
+function cancel_del(){
+    bac3()
+}
+//ปุ่มยืนยันการลบข้อมูล
+function confirm_del(){
     alert("ทำการยกเลิกการนัดหมายสำเร็จ")
+    bac3()
 }
 
-//กดดูแล้วแสดง Popup view
+//กดดู แล้วแสดง Popup view
 document.querySelectorAll('.info').forEach(a => {
         a.addEventListener('click', function() {
             //ให้แยกกับปุ่ม
@@ -34,7 +46,7 @@ document.querySelectorAll('.info').forEach(a => {
     });
 });
 
-//กดปุ่มแก้ไขแล้วแสดง Popup edit
+//กดปุ่มแก้ไข แล้วแสดง Popup edit
 document.querySelectorAll('.edit-button').forEach(a => {
         a.addEventListener('click', function() {
             event.stopPropagation(); // กันไว้ให้แยกจากการกดแถว
@@ -43,4 +55,15 @@ document.querySelectorAll('.edit-button').forEach(a => {
         document.getElementById("popup-edit").style.opacity = 1;
         document.getElementById("popup-ov").style.opacity = 1;
     });
+});
+
+//กดปุ่มลบ แล้วแสดง Popup edit
+document.querySelectorAll('.delete-button').forEach(a => {
+    a.addEventListener('click', function() {
+        event.stopPropagation(); // กันไว้ให้แยกจากการกดแถว
+    document.getElementById("popup-delete").style.visibility = "visible";
+    document.getElementById("popup-ov").style.visibility = "visible";
+    document.getElementById("popup-delete").style.opacity = 1;
+    document.getElementById("popup-ov").style.opacity = 1;
+});
 });
