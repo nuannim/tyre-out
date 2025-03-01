9bcee886aebaa411ed756dac3a896b8686fe68f3
mrakkan <mansuperyoung4399@gmail.com>
2025-03-01 22:21:00 +0700

    fixing-วัดระยะ

--- public/index.js
+++ public/index.js
@@ -145,6 +145,41 @@ function checkshowpop(){
     
 }
 
+function checkshowpopguest(){
+    const div = document.createElement("div");
+    div.id = "check-result";
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const th1 = document.createElement("th");
+    const th2 = document.createElement("th");
+    const th3 = document.createElement("th");
+    th1.textContent = "รายการเคมีภัณฑ์และอะไหล่";
+    th2.textContent = "จำนวน";
+    th3.textContent = "ราคา";
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    div.appendChild(table);
+
+    const divbtn = document.createElement("div");
+    divbtn.id = "check-btn";
+    // divbtn.innerHTML = '<button class="btn-pop" id="btn-pop" onclick="gotoapp()">นัดหมาย</button>';
+    divbtn.innerHTML = '<button class="btn-pop" id="btn-pop" onclick="gotoapp()">นัดหมาย</button>';
+    div.appendChild(divbtn);
+
+    document.getElementById("content").innerHTML = "";
+    document.getElementById("content").appendChild(div);
+    // if (sel1.value != "" & sel2.value != "" & sel3.value != "" & sel4.value != ""){
+    //     showpop()
+    // }
+    
+    showpop();
+    
+}
+
 function gotoapp(){
     // ไว้ส่ง value ของ dropdown ไปหน้า appointment
     // const sel1 = document.getElementById("sel1").value;
