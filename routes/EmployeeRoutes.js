1fde62210dd553d7bb5bd571f737dacc5aec8428
mrakkan <mansuperyoung4399@gmail.com>
2025-03-01 15:45:23 +0700

    for cj

--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -4,5 +4,6 @@ const authMiddleware = require('../middleware/auth');
 module.exports = function(app) {
     // app.get('/', EmployeeController.getIndexPage);
     app.get('/admin', authMiddleware, EmployeeController.getadminPage);
+    app.get('/admin_assign', authMiddleware, EmployeeController.getadminassignPage);
     // app.get('/history', EmployeeController.getHistoryPage);
 };
