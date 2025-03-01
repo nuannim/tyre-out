552b9809cdf3c4a6950996ce8d5aa5a0c34b8138
nuannim <66070286@kmitl.ac.th>
2025-03-02 02:22:38 +0700

    แก้ db & server.js นิดหน่อย
    ห้ามแตะ file server.js

--- server.js
+++ server.js
@@ -28,28 +28,55 @@ require('./routes/EmployeeRoutes.js')(app);
 
 // ! ชั่วคราว ใช้เพิ่ม database
 app.get('/create', (req, res) => {
-    for (let i = 0; i < 16; i++) {
-        // ตะกั่ว
-        db.run(`insert into MaintenanceGoods (maintenanceId, goodsId)
-            values (?, ?)`, [i, 18], (err, result) => {
-            if (err) {
-                console.error(err);
-                return res.send('Error creating database');
-            }
-        });
-
-        // if (i % 2 === 0) {
-            
-        // }
-        // db.run(`insert into`, (err, result) => {
+    for (let i = 1; i <= 16; i++) {
+        // * ตะกั่ว
+        // db.run(`insert into MaintenanceGoods (maintenanceId, goodsId)
+        //     values (?, ?)`, [i, 18], (err, result) => {
         //     if (err) {
         //         console.error(err);
         //         return res.send('Error creating database');
         //     }
-        //     res.send('Database created');
         // });
+
+        if (i % 2 === 0) {
+            db.run(`insert into MaintenanceGoods (maintenanceId, goodsId)
+                values (?, ?)`, [i, 8], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.send('Error creating database');
+                }
+            });
+        }
+        if (i % 3 === 0) {
+            db.run(`insert into MaintenanceGoods (maintenanceId, goodsId)
+                values (?, ?), (?, ?)`, [i, 6, i, 7], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.send('Error creating database');
+                }
+            });
+        }
+        if (i % 5 === 0) {
+            db.run(`insert into MaintenanceGoods (maintenanceId, goodsId)
+                values (?, ?)`, [i, 5], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.send('Error creating database');
+                }
+            });
+        }
+        if (i % 10 === 0) {
+            db.run(`insert into MaintenanceGoods (maintenanceId, goodsId)
+                values (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?)
+                , (?, ?), (?, ?)`, 
+                [i, 9, i, 10, i, 11, i, 12, i, 13, i, 14, i, 15, i, 16, i, 17], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.send('Error creating database');
+                }
+            });
+        }
     }
-    
 });
 
 
